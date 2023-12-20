b252e6118d033f583730c50265fa5af5b0b3e952
estefanoquiriconi97 <estefanoquiriconi@gmail.com>
2023-12-20 17:54:15 -0300

    Componentes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom/client';
+import {Greeting, UserCard} from './Greeting';
+import Product, {Navbar} from './Product'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDom.createRoot(document.getElementById("root"));
+
+
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <Greeting />
+    <UserCard/>
+    <Product/>
+    <Navbar/>
+  </>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
