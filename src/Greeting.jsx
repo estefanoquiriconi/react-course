b25b5de7074006171235e99f84bb7b6a8d1062ea
estefanoquiriconi97 <estefanoquiriconi@gmail.com>
2023-12-21 11:54:06 -0300

    Props, PropTypes y defaultProps, Estilos, Tipos de Componentes

--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -1,11 +1,22 @@
-export function Greeting() {
+export function Greeting({ title, name = "User" }) {
   return (
     <h1>
-        Componente de React
+      {title} dice {name}
     </h1>
   );
 }
 
-export function UserCard(){
-    return <h1>User Card</h1>
+export function UserCard({ name, amount, married, address, greet }) {
+  return (
+    <div>
+      <h1> {name} </h1>
+      <p> 💵{amount} </p>
+      <p>{married ? "married" : "sindle"}</p>
+      <ul>
+        <li>City: {address.city} </li>
+        <li>Address: {address.street} </li>
+        <li></li>
+      </ul>
+    </div>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDom from 'react-dom/client';
-import {Greeting, UserCard} from './Greeting';
-import Product, {Navbar} from './Product'
+import React from "react";
+import ReactDom from "react-dom/client";
+import { Greeting, UserCard } from "./Greeting";
+import Product, { Navbar } from "./Product";
+import {Button} from './Button';
+import {TaskCard} from './Task'
+import {Saludar} from './Saludar'
 
 const root = ReactDom.createRoot(document.getElementById("root"));
 
-
-
 root.render(
   <>
-    <Greeting />
-    <UserCard/>
-    <Product/>
-    <Navbar/>
+  <TaskCard ready={false} ></TaskCard>
+  <Saludar></Saludar>
   </>
 );
