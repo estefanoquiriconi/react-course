cb331ece53a8bdb1a571abe34ada3e63bd7bdeed
estefanoquiriconi97 <estefanoquiriconi@gmail.com>
2023-12-23 14:26:04 -0300

    event handlers

---
+++ src/Posts.jsx
@@ -0,0 +1,6 @@
+const Post = () => {
+    return <ul>
+        <li>Titulo de una publicación </li>
+        <li>Titulo de una publicación 2</li>
+    </ul>
+}
--- src/index.js
+++ src/index.js
@@ -8,9 +8,25 @@ import {Saludar} from './Saludar'
 
 const root = ReactDom.createRoot(document.getElementById("root"));
 
+const handleChange = (e) => {
+  console.log(e.target.value);
+}
+
 root.render(
   <>
   <TaskCard ready={false} ></TaskCard>
   <Saludar></Saludar>
+  <Button text="Saludar" name="Estéfano"></Button>
+  <Button text="Cambiar fondo" ></Button>
+
+  <input id="hola" onChange={handleChange}></input>
+
+  <form onSubmit={(e)=>{
+    e.preventDefault()
+    console.log('enviado');
+  }}>
+    <h1>Registro de usuario</h1>
+    <button>Send</button>
+  </form>
   </>
 );
